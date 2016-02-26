f1117d4be86c9f4fcea91e553594772393ec8370
dcyr <cyr.dominic@gmail.com>
2016-02-26 15:56:56 -0500

    modified:   PicusToLandisAssembly.R

--- PicusToLandisAssembly.R
+++ PicusToLandisAssembly.R
@@ -34,7 +34,7 @@ x <- x[grep("growthParam_", x)]
 #### subsample of folderNames
 #folderNames <- folderNames[grep("Acadian", folderNames)]#"AM|BSE|BSW|BP"
 areas <- unique(gsub("growthParam_|.RData", "", x))
-timestep <- 5
+timestep <- 10
 
 #### configuration of landis Climate Change scenarios
 landisCCScenarios <- list(RCP85 = list("0" = c("Baseline", "Baseline"),
@@ -57,7 +57,7 @@ for (a in areas) {
 
     #### assembling parameter table according to landis format
     growthParam <- get(load(paste0("growthParam_", a, ".RData")))
-    sep <- get(load(paste0("sep_", a, "_5yrsTS.RData")))
+    sep <- get(load(paste0("sep_", a, ".RData")))
     biomassSuccessionDynamicParams <- list()
 
 
